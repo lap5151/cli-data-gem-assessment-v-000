de4de19fcbc9ae31c2caac7e85abd9db3483079a
Lauren Pardee <lap5151@gmail.com>
2018-08-25 16:53:44 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
 
   def self.scrape_index_page(index_url = "https://www.theworlds50best.com/list/1-50-winners")
     doc = Nokogiri::HTML(open(index_url))
-    puts website
+    doc.css("div#t1-50 li")
     binding.pry
   end
 
