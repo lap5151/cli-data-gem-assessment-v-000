9a42aa8326515ee4a296061788a99771f0ecd129
Lauren Pardee <lap5151@gmail.com>
2018-08-25 16:41:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url = 'https://www.tripadvisor.com/')
-    website = Nokogiri::HTML(open(index_url))
+    doc = Nokogiri::HTML(open(index_url))
     puts website
     binding.pry
   end
